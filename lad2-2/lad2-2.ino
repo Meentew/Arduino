1b9fc9f6cad4bcf23f5363a21d5dd8e9ebeaa41b
Meentew <palen2555@gmail.com>
2023-06-16 16:11:31 +0700

    update

--- lad2-2/lad2-2.ino
+++ lad2-2/lad2-2.ino
@@ -1,14 +1,6 @@
-
 int lok[]={16,5,4,0,2};
-void setup() {
-pinMode (lok[0], OUTPUT);
-pinMode (lok[1], OUTPUT);
-pinMode (lok[2], OUTPUT);
-pinMode (lok[3], OUTPUT);
-pinMode (lok[4], OUTPUT);
-}
-void loop() {
-  for(int i=0; i<=3;i++){
+void tew01(){
+for(int i=0; i<=3;i++){
   digitalWrite(lok[i], 1);
   delay(500);
   digitalWrite(lok[i], 0);
@@ -18,5 +10,16 @@ void loop() {
   digitalWrite(lok[4], 1);
   delay(500);
   digitalWrite(lok[4], 0);
+
+}
+void setup() {
+pinMode (lok[0], OUTPUT);
+pinMode (lok[1], OUTPUT);
+pinMode (lok[2], OUTPUT);
+pinMode (lok[3], OUTPUT);
+pinMode (lok[4], OUTPUT);
+}
+void loop() {
+ tew01();
   
 }
